d48672d54b235efd530f6e810315a25ad8209d67
Al-Mawadda-Apps <almawadda.app@gmail.com>
2025-06-15 16:09:52 +0300

    v51

---
+++ src/views/logs/AutoUpdatesView.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="ComponentWrapper">
+    <div class="MButton" id="ReloadAutoUpdatesLogBTN">اعادة تحميل البيانات</div>
+    <MTable ref="AutoUpdatesLogTB" :Name="'AutoUpdatesLogTB'" :DataArray="AutoUpdatesLogTBData"
+      :Columns="AutoUpdatesLogTBColumns" :GetDataFunction="GetAutoUpdatesLogData" :RowsCount="AutoUpdatesLogTBRowsCount"
+      :RowsPerPage="10">
+    </MTable>
+  </div>
+</template>
+
+<!-- ======================================== -->
+<script>
+import { ref } from 'vue';
+import { api } from '../../axios';
+import { useAuthStore } from '../../stores/auth';
+import { useGlobalsStore } from '../../stores/Globals.js';
+
+export default {
+  setup() {
+    const authStore = useAuthStore()
+    const GlobalsStore = ref(useGlobalsStore());
+    const hasPermission = permission =>
+      authStore.user && authStore.user.permissions.includes(permission)
+
+    return {
+      GlobalsStore,
+
+      hasPermission,
+
+      AutoUpdatesLogTB: ref(null),
+      AutoUpdatesLogTBData: ref([]),
+      AutoUpdatesLogTBColumns: [
+        {
+          name: 'id',
+          label: '#',
+        },
+        {
+          name: 'compound',
+          label: 'المدينة',
+          filter: 'combo',
+          filter_items: GlobalsStore.value.ComboBoxes?.Compounds || [],
+        },
+        {
+          name: 'houseno',
+          label: 'العنوان',
+        },
+        {
+          name: 'buyername',
+          label: 'اسم المشتري',
+        },
+        {
+          name: 'buyerphone',
+          label: 'رقم هاتف المشتري',
+        },
+        {
+          name: 'realname',
+          label: 'اسم الساكن',
+        },
+        {
+          name: 'realnamephone',
+          label: 'رقم هاتف الساكن',
+        },
+        {
+          name: 'housestate',
+          label: 'حالة السكن',
+        },
+        {
+          name: 'sendfrom',
+          label: 'القسم',
+        },
+        {
+          name: 'actionname',
+          label: 'العملية',
+        },
+        {
+          name: 'execution',
+          label: 'التنفيذ',
+        },
+        {
+          name: 'action_date',
+          label: 'التاريخ',
+          filter: 'date',
+        },
+        {
+          name: 'action_time',
+          label: 'الوقت',
+        },
+      ],
+      AutoUpdatesLogTBRowsCount: ref(0),
+    }
+  },
+  mounted() {
+    this.AutoUpdatesLogTB.LoadMTable()
+
+    let Instance = this;
+
+    document.getElementById('ReloadAutoUpdatesLogBTN').addEventListener('click', function () {
+      Instance.AutoUpdatesLogTB.ReLoadMTable();
+    });
+  },
+  methods: {
+    GetAutoUpdatesLogData(MTable) {
+      api
+        .get('GetAutoUpdatesLogData', {
+          params: {
+            MTable: MTable,
+          },
+        })
+        .then(response => {
+          this.AutoUpdatesLogTBData = response.data.data
+          this.AutoUpdatesLogTBRowsCount = response.data.total
+        })
+        .catch(error => {
+          window.ShowMessage('حدث خطا', error)
+        })
+    },
+  },
+}
+</script>
