a4e418d63e0d259ad56d6684c5611f39115e0ecd
isaac <isaac@isaacs-MacBook-Pro.local>
2025-07-07 10:37:21 +0300

    v54

--- src/views/users/deeratnaAppUsers.vue
+++ src/views/users/deeratnaAppUsers.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="ComponentWrapper">
     <!-- ===============  add subsc Model  ========= -->
-    <MModal ref="AddCustomerModal" :Name="'AddCustomerModal'" :Title="'اضافة مشترك جديد'">
+    <MModal
+      ref="AddCustomerModal"
+      :Name="'AddCustomerModal'"
+      :Title="'اضافة مشترك جديد'"
+    >
       <!-- <MComboBox
         ref="Compounds"
         :Name="'Compounds'"
@@ -47,26 +51,46 @@
 
     <div class="MButton" id="GetCustomersBTN">عرض البيانات</div>
     <div class="MGroup">
-      <MDate ref="CustomersFromDate" :Name="'CustomersFromDate'" :Label="'تاريخ من'"></MDate>
-      <MDate ref="CustomersToDate" :Name="'CustomersToDate'" :Label="'تاريخ الى'"></MDate>
+      <MDate
+        ref="CustomersFromDate"
+        :Name="'CustomersFromDate'"
+        :Label="'تاريخ من'"
+      ></MDate>
+      <MDate
+        ref="CustomersToDate"
+        :Name="'CustomersToDate'"
+        :Label="'تاريخ الى'"
+      ></MDate>
     </div>
-    <MTable ref="CustomersTB" :Name="'CustomersTB'" :DataArray="CustomersTBData" :Columns="CustomersTBColumns"
-      :Sums="CustomersTBSums" :GetDataFunction="GetCustomersData" :RowsCount="CustomersTBRowsCount" :RowsPerPage="10">
+    <MTable
+      ref="CustomersTB"
+      :Name="'CustomersTB'"
+      :DataArray="CustomersTBData"
+      :Columns="CustomersTBColumns"
+      :Sums="CustomersTBSums"
+      :GetDataFunction="GetCustomersData"
+      :RowsCount="CustomersTBRowsCount"
+      :RowsPerPage="10"
+    >
       <template v-slot:options>
         <!-- View Videosdffhroif Option -->
         <div class="MTableOption" OptionEventName="EditItem">
           <div class="MTableOptionIcon">
             <svg viewBox="0 0 1000 1000">
-              <path d="M500.2,249.6c124.1,1.1,233.2,42.7,328.2,122.1c39.6,33,72.3,72.7,106.8,110.6c9,9.9,8.7,25.5-0.8,36.4
+              <path
+                d="M500.2,249.6c124.1,1.1,233.2,42.7,328.2,122.1c39.6,33,72.3,72.7,106.8,110.6c9,9.9,8.7,25.5-0.8,36.4
   c-53.9,62.1-109.3,122.5-182.1,163.7c-66,37.4-136.2,60.8-212,66.4C399.6,759.2,276.4,716.6,169,626.3
   C130.9,594.3,99.2,556,66,519.3c-10-11-10.5-26-0.9-37.1c53.3-61.5,107.9-121.4,179.6-162.7c66.8-38.5,138-62.1,215.1-68.3
   C473.2,250.2,486.7,250.1,500.2,249.6z M504.7,308.1c-19.4,0.8-34.9,0.7-50.4,2.2c-61.5,6.1-119,24.8-173,54.6
   c-59.2,32.7-106.2,79.4-150.3,129.6c-4.1,4.7-3.1,8.1,0.7,12.1c21.3,22.2,40.9,45.9,63.9,66.5c86.2,77.3,186.4,118.2,302.7,118.9
   c68.3,0.4,133-14.2,194.7-43.2c70.8-33.3,126.2-85.4,176.6-143.7c4-4.6,2-7.9-1.3-11.3c-21.3-22.2-40.9-45.9-63.8-66.6
-  C718.1,349.4,617.3,308.9,504.7,308.1z" />
-              <path d="M392.3,499.7c0.2-59,49.3-108.3,108.1-107.4c60.6,0.9,107.4,47.4,107.2,108c-0.2,60.9-47.5,106.9-107.8,107.4
+  C718.1,349.4,617.3,308.9,504.7,308.1z"
+              />
+              <path
+                d="M392.3,499.7c0.2-59,49.3-108.3,108.1-107.4c60.6,0.9,107.4,47.4,107.2,108c-0.2,60.9-47.5,106.9-107.8,107.4
   C440.8,608.1,392.1,558.6,392.3,499.7z M500.2,556.5c30.5,0,56.4-25.9,56.4-56.4c0-30.6-25.7-56.5-56.2-56.6
-  c-31-0.1-56.9,25.8-56.8,56.8C443.8,530.8,469.7,556.6,500.2,556.5z" />
+  c-31-0.1-56.9,25.8-56.8,56.8C443.8,530.8,469.7,556.6,500.2,556.5z"
+              />
             </svg>
           </div>
           <div class="MTableOptionName">تعديل</div>
@@ -74,16 +98,20 @@
         <div class="MTableOption" OptionEventName="DeleteItem">
           <div class="MTableOptionIcon">
             <svg viewBox="0 0 1000 1000">
-              <path d="M500.2,249.6c124.1,1.1,233.2,42.7,328.2,122.1c39.6,33,72.3,72.7,106.8,110.6c9,9.9,8.7,25.5-0.8,36.4
+              <path
+                d="M500.2,249.6c124.1,1.1,233.2,42.7,328.2,122.1c39.6,33,72.3,72.7,106.8,110.6c9,9.9,8.7,25.5-0.8,36.4
   c-53.9,62.1-109.3,122.5-182.1,163.7c-66,37.4-136.2,60.8-212,66.4C399.6,759.2,276.4,716.6,169,626.3
   C130.9,594.3,99.2,556,66,519.3c-10-11-10.5-26-0.9-37.1c53.3-61.5,107.9-121.4,179.6-162.7c66.8-38.5,138-62.1,215.1-68.3
   C473.2,250.2,486.7,250.1,500.2,249.6z M504.7,308.1c-19.4,0.8-34.9,0.7-50.4,2.2c-61.5,6.1-119,24.8-173,54.6
   c-59.2,32.7-106.2,79.4-150.3,129.6c-4.1,4.7-3.1,8.1,0.7,12.1c21.3,22.2,40.9,45.9,63.9,66.5c86.2,77.3,186.4,118.2,302.7,118.9
   c68.3,0.4,133-14.2,194.7-43.2c70.8-33.3,126.2-85.4,176.6-143.7c4-4.6,2-7.9-1.3-11.3c-21.3-22.2-40.9-45.9-63.8-66.6
-  C718.1,349.4,617.3,308.9,504.7,308.1z" />
-              <path d="M392.3,499.7c0.2-59,49.3-108.3,108.1-107.4c60.6,0.9,107.4,47.4,107.2,108c-0.2,60.9-47.5,106.9-107.8,107.4
+  C718.1,349.4,617.3,308.9,504.7,308.1z"
+              />
+              <path
+                d="M392.3,499.7c0.2-59,49.3-108.3,108.1-107.4c60.6,0.9,107.4,47.4,107.2,108c-0.2,60.9-47.5,106.9-107.8,107.4
   C440.8,608.1,392.1,558.6,392.3,499.7z M500.2,556.5c30.5,0,56.4-25.9,56.4-56.4c0-30.6-25.7-56.5-56.2-56.6
-  c-31-0.1-56.9,25.8-56.8,56.8C443.8,530.8,469.7,556.6,500.2,556.5z" />
+  c-31-0.1-56.9,25.8-56.8,56.8C443.8,530.8,469.7,556.6,500.2,556.5z"
+              />
             </svg>
           </div>
           <div class="MTableOptionName">حذف</div>
@@ -133,18 +161,18 @@ export default {
           label: 'رمز المستخدم',
         },
         {
-          name: 'compound',
+          name: 'real_estates.compound',
           label: 'المدينة',
           filter: 'combo',
           filter_items: GlobalsStore.value.ComboBoxes?.Compounds || [],
         },
         {
-          name: 'username',
-          label: 'كلمة الدخول',
+          name: 'real_estates.address',
+          label: 'العنوان',
         },
         {
-          name: 'address',
-          label: 'العنوان',
+          name: 'username',
+          label: 'كلمة الدخول',
         },
         {
           name: 'phone',
@@ -178,7 +206,6 @@ export default {
         this.CustomersTB.ReLoadMTable()
       }.bind(this)
     )
-
     // EditItem
     document.getElementById('CustomersTB').addEventListener(
       'EditItem',
